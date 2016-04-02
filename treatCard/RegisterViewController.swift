9d2f1659d526227547351f0bf5c4084892d45c4a
baronkwan <baronkwan@outlook.com>
2016-04-02 03:31:01 -0700

    add alerts for register page

--- treatCard/RegisterViewController.swift
+++ treatCard/RegisterViewController.swift
@@ -10,10 +10,8 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
