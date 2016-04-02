2675c87529e608bce67372ee753234c586848755
ZenStein <shaneenterprises@gmail.com>
2016-04-01 18:09:32 -0700

    alamofire include, first http request works

--- treatCard/FirstViewController.swift
+++ treatCard/FirstViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://rhubarb-sundae-21254.herokuapp.com/")
+            .responseJSON { _, _, result in
+                print(result)
+                debugPrint(result)
+        }
+        
+        
     }
+    
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
